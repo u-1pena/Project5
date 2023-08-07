1a7137e9e0314cc94a17f04c1fd0ee4d39740d33
u-1 <shimaichi5973@gamil.com>
2023-08-07 22:31:27 +0900

    Dogクラスを追加とmainクラスにgetname()メソッドの追加

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,18 @@ public class Main {
         Cat cat2 = new Cat("tama");
         Cat cat3 = new Cat("mike");
 
+        Dog dog1 = new Dog("pochi");
+        Dog dog2 = new Dog("taro");
+        Dog dog3 = new Dog("hachi");
+
+
         System.out.println("Hello!" + cat1.getName() + "!");
         System.out.println("Hello!" + cat2.getName() + "!");
         System.out.println("Hello!" + cat3.getName() + "!");
+
+        System.out.println("Hello!" + dog1.getName() + "!");
+        System.out.println("Hello!" + dog2.getName() + "!");
+        System.out.println("Hello!" + dog3.getName() + "!");
+
     }
 }
